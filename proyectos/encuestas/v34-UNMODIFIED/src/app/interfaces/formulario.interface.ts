2abd447c53d854ce1e46b7253726f254b05f475b
sergio villalobos <55vias55@gmail.com>
2019-01-22 14:10:38 -0700

    added

--- proyectos/encuestas/v34-UNMODIFIED/src/app/interfaces/formulario.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface FormularioInterface {
-    bnt_aceptar?: string;
-    btn_cancelar?: string;
-    btn_comenzar?: string;
-    correo_electronico?: string;
-    nombre_asesor?: string;
-    nombre_cliente?: string;
-    region?: string;
-  }
